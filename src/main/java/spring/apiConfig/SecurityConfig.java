7a67f92fa001891f78ff705d24d348db41fff90b
jbernal <jbernal@etsisi.upm.es>
2016-01-15 18:53:25 +0100

    Security. Refactoring

--- src/main/java/spring/apiConfig/SecurityConfig.java
+++ src/main/java/spring/apiConfig/SecurityConfig.java
@@ -26,29 +26,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-        // auth.inMemoryAuthentication().//
-        // withUser("user").password("123456").roles("USER").and().//
-        // withUser("manager").password("123456").roles("MANAGER").and().//
-        // withUser("admin").password("123456").roles("ADMIN", "MANAGER", "USER");
+//        auth.inMemoryAuthentication().//
+//                withUser("user").password("123456").roles("USER").and().//
+//                withUser("manager").password("123456").roles("MANAGER").and().//
+//                withUser("admin").password("123456").roles("ADMIN", "MANAGER", "USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .authorizeRequests()
-                .antMatchers(Uris.SERVLET_MAP + Uris.ADMINS + "/**").permitAll()
+        http.csrf().disable().authorizeRequests().antMatchers(Uris.SERVLET_MAP + Uris.ADMINS + "/**").permitAll()
                 .antMatchers(Uris.SERVLET_MAP + Uris.SECURITY + Uris.USER + "/**").authenticated()
-                .antMatchers(HttpMethod.GET, Uris.SERVLET_MAP + Uris.SECURITY + Uris.ADMIN).hasRole("ADMIN")
-                .and()
-            .httpBasic();
+                .antMatchers(HttpMethod.GET, Uris.SERVLET_MAP + Uris.SECURITY + Uris.ADMIN).hasRole("ADMIN").and().httpBasic();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        PasswordEncoder encoder = new BCryptPasswordEncoder();
-        return encoder;
+        return new BCryptPasswordEncoder();
     }
 
-    // Para los servicios: @PreAuthorize("hasRole('ROLE_USER')")
 }
