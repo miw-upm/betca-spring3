cb81f9c7063a0644da6ac36ba89f5c620f6c4a62
jbernal <jbernal@etsisi.upm.es>
2016-02-23 09:14:47 +0100

    Refactoring Aspect

--- src/main/java/config/AspectConfig.java
+++ src/main/java/config/AspectConfig.java
@@ -1,9 +1,11 @@
 package config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @EnableAspectJAutoProxy
+@ComponentScan(basePackages = {"aspect","aspectTarget"})
 public class AspectConfig {
 }
