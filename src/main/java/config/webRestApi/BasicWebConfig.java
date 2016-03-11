6294c4d1f48e7c023af33fda46d042af0c301b2f
jbernal <jbernal@etsisi.upm.es>
2016-03-11 10:35:41 +0100

    Refactoring hello api

--- src/main/java/config/webRestApi/BasicWebConfig.java
+++ src/main/java/config/webRestApi/BasicWebConfig.java
@@ -1,9 +1,10 @@
-package config;
+package config.webRestApi;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import config.PackageNames;
 
 @Configuration
 @EnableWebMvc
